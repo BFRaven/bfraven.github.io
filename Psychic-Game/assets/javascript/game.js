a58be892ab34b1a768cd27e90fd16e51ff29dca7
BFRaven <37793087+BFRaven@users.noreply.github.com>
2018-05-30 18:43:35 -0500

    final Game HW assignment

--- Psychic-Game/assets/javascript/game.js
+++ Psychic-Game/assets/javascript/game.js
@@ -10,19 +10,38 @@ var compChoices = [
     var wins = 0;
     var losses = 0;
     var guessesLeft = 9;
+    var guessesFar = [];
 
     document.onkeyup = function(event) {
         
         var player = event.key;
 
         var compGuess = compChoices[Math.floor(Math.random() * compChoices.length)];
-    
-        if ((player === compChoices.length)) {
+    console.log("player= " + player + "compGuess= " + compGuess)
+        if (player === compGuess) {
+            wins++;
+            disUpdate();
+            }
+            else {
+                //console.log(guessesFar
+                losses++;
+                guessesLeft--;
+                guessesFar.push(player);
+                disUpdate();
+            } 
+    if (guessesLeft < 1) {
+          //wins = 0;
+          //losses= 0;
+          guessesLeft = 9;
+          guessesFar = [];
+       }
+          // I can keep if w/o else
+     
+      function disUpdate () {
+          document.getElementById("wins").innerHTML = wins
+          document.getElementById("losses").innerHTML = losses
+          document.getElementById("yourGuesses").innerHTML = guessesFar
+          document.getElementById("guessesLeft").innerHTML = guessesLeft
+      }
 
-           
-        }
-    
-    
-    
-    
-    }
+    };
